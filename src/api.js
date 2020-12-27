97aef38ad9222fa25359f0011272ffa87f4befcb
WekaMbali <klyntonmainya@gmail.com>
2020-12-28 00:16:47 +0300

    URL for api fetching

---
+++ src/api.js
@@ -0,0 +1,33 @@
+//Base URL
+const base_url = 'https://api.rawg.io/api/';
+
+//Getting the dates
+const getCurrentMonth = ()=>{
+    const month = new Date().getMonth() + 1;
+    if(month < 10){
+        return `0${month}`;
+    }else{
+        return month;
+    }
+};
+
+const getCurrentDay = ()=>{
+    const day = new Date().getDay();
+    if(day < 10){
+        return `0${day}`;
+    }else{
+        return day;
+    }
+};
+//Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
